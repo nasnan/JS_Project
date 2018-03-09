2d72f2492bd48fe4954d1b7d81077957832114dd
Zoey <nasnan77@outlook.com>
2018-03-09 10:57:03 +0800

    接受参数改变div属性值

---
+++ chapter2/agmchangediv/scripts/main.js
@@ -0,0 +1,30 @@
+function getargument(){
+	inputs=document.getElementsByTagName("input");
+	agms=[];
+	for(var i=0;i<inputs.length;i++){
+		agms[i]=inputs[i].value;
+	}
+	return agms;
+}
+
+function changediv(){
+	div=document.getElementById("thediv");
+	sbmt=document.getElementById("sbmt");
+	restart=document.getElementById("restart");
+	oldcn=div.className;
+	
+
+	sbmt.onclick=function(){
+		[name,value]=getargument();
+		if(name&&value)
+		{
+			div.style[name]=value;
+		}
+	}
+	restart.onclick=function(){
+		div.removeAttribute("style")
+	}
+}
+
+
+window.onload=changediv;
