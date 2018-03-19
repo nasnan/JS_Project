22410ebf5b498b332633a34345fb78026b7913a0
Zoey <nasnan77@outlook.com>
2018-03-19 19:38:31 +0800

    修改表达式

--- chapter3/webComputer/scripts/main.js
+++ chapter3/webComputer/scripts/main.js
@@ -14,18 +14,14 @@ $(document).ready(function(){
 		}
 		if(clk!="c" && clk!="=")		//当前按键非清空或等于
 		{
-			equation=returnEquation(equation,e.target,old);
-			big=nowBignum(big,e.target,old);
+			[equation,big]=returnEquation(equation,e.target,old,big);
 			$("#ans").text(big);
 		}
 		if(clk=="="){
-			var ans=0;
-			ans=eval(equation);
-			if(ans>9999999999 || ans<-9999999999)
-				ans="too long";
-			if(ans.toString().length>10)
-				ans=ans.toString().slice(0,11)
-			$("#ans").text(ans);
+		if (!/[\d]/.test(equation.slice(-1))){
+			equation=equation.slice(0,-1);
+		}
+			ans=returnAns(equation);
 		}
 		if(clk=="c"){
 			equation="";
@@ -33,30 +29,39 @@ $(document).ready(function(){
 			$("#ans").text("0");
 		}
 		$("#equation").text(equation);
-
-
-
 	})
 })
 
 
-function returnEquation(equation,symbolnow,symbolold){
+function returnEquation(equation,symbolnow,symbolold,big){
 	var symbolhtml=symbolnow.innerHTML;
 
 	if($(symbolnow).attr("class").indexOf("count")!=-1){	//当前按键为运算符号
 		$(".dot").attr("dotted","false");	//设置当前数没有小数点
+		if(equation.length==0) {
+			equation+="0";	//如果是开头则先增加0
+			big+="0";
+		}
 		if(equation.slice(-1)=="." ||
 		($(symbolold).attr("class") && 	//上一按键也是运算符号则替换
 		$(symbolold).attr("class").indexOf("count")!=-1)){	//且不是等号
 			equation=equation.slice(0,-1)+symbolhtml;
+			
 		}
 		else{
 			equation+=symbolhtml;
 		}
+		big=symbolhtml;
 	}
 
 	if(/[0-9]/.test(symbolhtml)){	//当前按键为数字
 		equation+=symbolhtml;
+		if(/[0-9|\.]/.test(symbolold.text())){
+			big+=symbolhtml;
+		}
+		else{
+			big=symbolhtml;	
+		}
 	}
 
 	if(/[.]/.test(symbolhtml)){		//当前按键为小数点
@@ -65,36 +70,29 @@ function returnEquation(equation,symbolnow,symbolold){
 			$(".dot").attr("dotted","true");	//设置该数已有小数点
 			if(/[^(0-9)]/.test(equation.slice(-1)) ||	//前面字符串没有0
 			equation==""){	//算式为空
-				equation+="0."
+				equation+="0.";
+				big="0.";
 			}
 			else{
-				equation+="."
+				equation+=".";
+				if(/[0-9|\.]/.test(equation.slice(-1))){
+					big+=symbolhtml;
+				}
 			}
 		}
 	}
-	return equation;
+	return [equation,big];
 }
 
-function nowBignum(big,symbolnow,symbolold){
-	var nowtext=symbolnow.innerHTML;
-	if(!symbolold.text()){	//刚刚开始
-		big=nowtext;
-	}
-	else{
-		var oldcls=symbolold.attr("class");
-		var nowcls=symbolnow.className;
-		if(nowcls.indexOf(oldcls)!=-1){		//类型和前面一样
-			if(oldcls.indexOf("num")!=-1){	//都是数字
-				big+=nowtext;
-			}
-			if(oldcls.indexOf("count")!=-1){	//都是符号
-				big=nowtext;
-			}
-		}
-		else{	//类型不同替换
-			big=nowtext;
-		}
-
-	}
-	return big;
+function returnAns(equation){
+	var ans=0;
+	
+	ans=eval(equation);
+	if(equation.length==0) ans=0;
+	if(ans>9999999999 || ans<-9999999999)
+		ans="too long";
+	if(ans.toString().length>10)
+		ans=ans.toString().slice(0,11)
+	$("#ans").text(ans);
+	return ans;
 }
