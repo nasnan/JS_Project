62d542e002d8f44336a4fd520830e1c0c726b33f
Zoey <nasnan77@outlook.com>
2018-03-22 08:25:23 +0800

    点开时如果有已展开的 将其收起

--- index/scripts/main.js
+++ index/scripts/main.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
 
 	$("dt").click(function(e){
 		if($(this).attr("class") &&
-		$(this).attr("class").indexOf("now")!=-1){
+		$(this).attr("class").indexOf("now")!=-1){	//单击已点开的收起
 			$(this).removeClass("now");
 			$(this).nextUntil("dt").slideUp(500);
 		}
 		else{
-			$(this).nextUntil("dt").slideDown(500);
+			if($(".now")!=$(this)){		//点开新的收起其他已点开的
+				$(".now").nextUntil("dt").slideUp(500);
+				$(".now").removeClass("now");
+			}
+			$(this).nextUntil("dt").slideDown(500);		//展开新的
 			$(this).addClass("now");
 		}
 		
