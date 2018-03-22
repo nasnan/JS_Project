d86a171b8334fdecce71a4623fcffd4861183183
Zoey <nasnan77@outlook.com>
2018-03-22 11:05:15 +0800

    update flash

--- chapter3/countDownClock/scripts/main.js
+++ chapter3/countDownClock/scripts/main.js
@@ -4,17 +4,17 @@ $(document).ready(function(){
 	var timecountdown=setInterval(setCountDown,1000);
 	var timeflash;
 	function setCountDown(){
-		
 		$("p").text($("p").text()-1);
 		if(parseInt($("p").text())==3){
 			timeflash=setInterval(flash,500);
 			$($("p")).addClass("three");
 		}
-		if(parseInt($("p").text())<0){
-
-			clearInterval(timeflash);
+		if(parseInt($("p").text())==0){
 			clearInterval(timecountdown);
-			alert("时间到");
+			setTimeout(function(){
+				clearInterval(timeflash);
+			},500)
+			
 		}
 	}	
 
@@ -28,10 +28,10 @@ $(document).ready(function(){
 function flash(){
 	if($("p").attr("flag")=="false"){
 		$("p").attr("flag","true");
-		$("p").css("display","none");
+		$("p").css("display","block");
 	}
 	else{
 		$("p").attr("flag","false");
-		$("p").css("display","block");
+		$("p").css("display","none");
 	}
 }
