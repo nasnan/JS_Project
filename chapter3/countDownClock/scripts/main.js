1beccfb95feac7e449950661db4ce042c00cf53c
Zoey <nasnan77@outlook.com>
2018-03-22 10:49:23 +0800

    增加倒数三秒红色闪闪烁

--- chapter3/countDownClock/scripts/main.js
+++ chapter3/countDownClock/scripts/main.js
@@ -1,11 +1,37 @@
 $(document).ready(function(){
-	timecountdown=setInterval(setCountDown,1000);
+	$("p").attr("flag","false");
+
+	var timecountdown=setInterval(setCountDown,1000);
+	var timeflash;
+	function setCountDown(){
+		
+		$("p").text($("p").text()-1);
+		if(parseInt($("p").text())==3){
+			timeflash=setInterval(flash,500);
+			$($("p")).addClass("three");
+		}
+		if(parseInt($("p").text())<0){
+
+			clearInterval(timeflash);
+			clearInterval(timecountdown);
+			alert("时间到");
+		}
+	}	
+
+
 })
 
 
-function setCountDown(){
-	$("p").text($("p").text()-1);
-	if(parseInt($("p").text())<=0){
-		clearInterval(timecountdown);
+
+
+
+function flash(){
+	if($("p").attr("flag")=="false"){
+		$("p").attr("flag","true");
+		$("p").css("display","none");
+	}
+	else{
+		$("p").attr("flag","false");
+		$("p").css("display","block");
 	}
 }
